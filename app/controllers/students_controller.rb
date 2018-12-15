292ab8ac109d245e97b3ec8d531b90f5275ccecb
Michael Reinders <Michael.H.Reinders@gmail.com>
2018-12-15 10:48:44 -0800

    Added delete and update features

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,14 +8,40 @@ class StudentsController < ApplicationController
 	end
 
 	def new
+		@student = Student.new
+	end
 
+	def edit 
+		@student = Student.find(params[:id])
 	end
 
 	def create 
 		#render plain: params[:student].inspect
-		@student = Student.new(params.require(:student).permit(:last, :first, :gpa, :date))
+		@student = Student.new(student_params)
 
 		@student.save
 		redirect_to @student
 	end
+
+	def update
+		@student = Student.find(params[:id])
+
+		if @student.update(student_params)
+			redirect_to @student
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@student = Student.find(params[:id])
+		@student.destroy
+
+		redirect_to students_path
+	end
+
+	private 
+		def student_params
+			params.require(:student).permit(:last, :first, :gpa, :date)
+		end
 end
